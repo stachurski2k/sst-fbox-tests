1d89e1870d162df24965abeaeea67b890364941e
username98673 <148317329+username98673@users.noreply.github.com>
2025-05-26 13:19:38 +0200

    dzien5_4

--- app/src/main/java/com/example/sst_fbox_tests/ChoiceLogger.java
+++ app/src/main/java/com/example/sst_fbox_tests/ChoiceLogger.java
@@ -1,6 +1,13 @@
 package com.example.sst_fbox_tests;
+import android.content.ContentValues;
 import android.content.Context;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -16,13 +23,29 @@ public class ChoiceLogger {
     public static void appendChoices(Context context, String choices) {
         if (fileName == null) return;
         try {
-            FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_APPEND);
+            OutputStream outputStream;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Downloads.DISPLAY_NAME, fileName);
+                values.put(MediaStore.Downloads.MIME_TYPE, "text/plain");
+                values.put(MediaStore.Downloads.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS);
+
+                outputStream = context.getContentResolver()
+                        .openOutputStream(
+                                context.getContentResolver()
+                                        .insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values),
+                                "wa" // "wa" to append mode
+                        );
+            } else {
+                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+                outputStream = new FileOutputStream(file, true);
+            }
+
             String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
-            fos.write((timestamp + ": " + choices + "\n").getBytes());
-            fos.close();
+            outputStream.write((timestamp + ": " + choices + "\n").getBytes());
+            outputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
