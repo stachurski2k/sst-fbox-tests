b4445e87f6410b18fbaec61bf250b1debfe98662
YoiSenshu <101147494+YoiSenshu@users.noreply.github.com>
2025-07-16 23:50:57 +0200

    add action bar with title

--- app/src/main/java/pl/vrtechnology/fboxtester/settings/SettingsActivity.java
+++ app/src/main/java/pl/vrtechnology/fboxtester/settings/SettingsActivity.java
@@ -1,8 +1,11 @@
 package pl.vrtechnology.fboxtester.settings;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -16,6 +19,23 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setTitle(R.string.settings_title);
+        }
+
         SettingsViewModel viewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getOnBackPressedDispatcher().onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
