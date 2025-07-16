d33604a8d909946e4c95f10f29e635614fbe7ce7
YoiSenshu <101147494+YoiSenshu@users.noreply.github.com>
2025-07-16 19:31:50 +0200

    SettingsRepository

---
+++ app/src/main/java/pl/vrtechnology/fboxtester/settings/SettingsRepository.java
@@ -0,0 +1,50 @@
+package pl.vrtechnology.fboxtester.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+
+import org.greenrobot.eventbus.EventBus;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.qualifiers.ApplicationContext;
+
+public class SettingsRepository {
+
+    private static final String PREFS_NAME = "app_settings";
+    private static final String KEY_SERVER_ADDRESS = "server_address";
+    private static final String KEY_SERVER_PORT = "server_port";
+
+
+    private static final String DEFAULT_SERVER_ADDRESS = "172.20.0.226";
+    private static final int DEFAULT_SERVER_PORT = 8080;
+
+    private final SharedPreferences sharedPreferences;
+
+    @Inject
+    public SettingsRepository(@ApplicationContext @NonNull Context context) {
+        this.sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+    }
+
+    public String getServerAddress() {
+        return sharedPreferences.getString(KEY_SERVER_ADDRESS, DEFAULT_SERVER_ADDRESS);
+    }
+
+    public void setServerAddress(String address) {
+        sharedPreferences.edit().putString(KEY_SERVER_ADDRESS, address).apply();
+        EventBus.getDefault().post(new SettingsChangedEvent());
+    }
+
+    public int getServerPort() {
+        return sharedPreferences.getInt(KEY_SERVER_PORT, DEFAULT_SERVER_PORT);
+    }
+
+    public void setServerPort(int port) {
+        sharedPreferences.edit().putInt(KEY_SERVER_PORT, port).apply();
+        EventBus.getDefault().post(new SettingsChangedEvent());
+    }
+
+    public static class SettingsChangedEvent {}
+}
