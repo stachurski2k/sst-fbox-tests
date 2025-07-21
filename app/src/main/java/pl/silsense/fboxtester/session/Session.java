c3f96a2583edfec05de02a8f60f85146f293e712
YoiSenshu <101147494+YoiSenshu@users.noreply.github.com>
2025-07-22 01:19:43 +0200

    add todos

--- app/src/main/java/pl/silsense/fboxtester/session/Session.java
+++ app/src/main/java/pl/silsense/fboxtester/session/Session.java
@@ -10,3 +10,22 @@ public interface Session extends Serializable {
 
     DocumentFile getFile();
 }
+
+/*
+* TODO:
+* Przenieść stringi z Session manager activity do stringów w zasobach.
+* Zmienić system nazw plików sesji, ustalić jak działa nazwa sesji.
+* Ulepszyć widok w Main Menu.
+* Przebudować LogEntry itd.
+* Zrobić fragmenty dla loggera:
+* - menu wyboru elementu
+* - menu z nazwami błędów
+* - wybór miejsca na ścianie
+* Podpiąć fragmenty do aktywności loggera.
+* Sprawdzić czy logger działa.
+* Zrobić aktywność statystyk z:
+* - menu wybieranie sesji
+* - widok statystyk
+* + przycisk w menu głównym do statystyk.
+* Dodać wysyłanie przez UDP.
+* */
