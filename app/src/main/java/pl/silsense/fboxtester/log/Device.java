fcaf1d5527b24c352176c4edc0944022656eccc8
YoiSenshu <wojteklyczba1@gmail.com>
2025-07-23 10:45:08 +0200

    device names from enum

--- app/src/main/java/pl/silsense/fboxtester/log/Device.java
+++ app/src/main/java/pl/silsense/fboxtester/log/Device.java
@@ -1,23 +1,26 @@
 package pl.silsense.fboxtester.log;
 
+import androidx.annotation.StringRes;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import pl.silsense.fboxtester.R;
 
 @Getter
+@AllArgsConstructor
 public enum Device {
-    WALL_1(true),
-    WALL_2(true),
-    WALL_3(true),
-    WALL_4(true),
-    THROWER_1(false),
-    THROWER_2(false),
-    THROWER_3(false),
-    THROWER_4(false),
-    THROWER_5(false),
-    THROWER_6(false);
+    WALL_1(true, R.string.logger_wall_1_name),
+    WALL_2(true, R.string.logger_wall_2_name),
+    WALL_3(true, R.string.logger_wall_3_name),
+    WALL_4(true, R.string.logger_wall_4_name),
+    THROWER_1(false, R.string.logger_thrower_1_name),
+    THROWER_2(false, R.string.logger_thrower_2_name),
+    THROWER_3(false, R.string.logger_thrower_3_name),
+    THROWER_4(false, R.string.logger_thrower_4_name),
+    THROWER_5(false, R.string.logger_thrower_5_name),
+    THROWER_6(false, R.string.logger_thrower_6_name);
 
     public final boolean wall;
-
-    Device(boolean wall) {
-        this.wall = wall;
-    }
+    @StringRes
+    public final int name;
 }
