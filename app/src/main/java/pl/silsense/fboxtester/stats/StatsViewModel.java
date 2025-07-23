7803fcc33b15253ec278584b472ecbabe17a3c47
YoiSenshu <wojteklyczba1@gmail.com>
2025-07-23 12:15:43 +0200

    part of stats details

--- app/src/main/java/pl/silsense/fboxtester/stats/StatsViewModel.java
+++ app/src/main/java/pl/silsense/fboxtester/stats/StatsViewModel.java
@@ -1,5 +1,6 @@
 package pl.silsense.fboxtester.stats;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -8,7 +9,9 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -16,7 +19,9 @@ import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
 import lombok.Getter;
+import pl.silsense.fboxtester.log.Device;
 import pl.silsense.fboxtester.log.LogEntry;
+import pl.silsense.fboxtester.log.LogType;
 import pl.silsense.fboxtester.session.Session;
 import pl.silsense.fboxtester.session.SessionRepository;
 import pl.silsense.fboxtester.util.ConsumableEvent;
@@ -85,6 +90,73 @@ public class StatsViewModel extends ViewModel {
      */
     // TODO implement
     public String getStatisticsContent() {
-        return "";
+        List<LogEntry> logEntries = getAllLogEntriesFromImportedSessions();
+        if(getAllLogEntriesFromImportedSessions().isEmpty()) {
+            return "Brak danych.";
+        }
+
+        StringBuilder content = new StringBuilder();
+
+        // walls
+        content.append("Ściana 1\n");
+        content.append(wallDetails(logEntries, Device.WALL_1));
+        content.append("Ściana 2\n");
+        content.append(wallDetails(logEntries, Device.WALL_2));
+        content.append("Ściana 3\n");
+        content.append(wallDetails(logEntries, Device.WALL_3));
+        content.append("Ściana 4\n");
+        content.append(wallDetails(logEntries, Device.WALL_4));
+        content.append("\n");
+
+        // thrower
+        content.append("Wyrzutnik 1\n");
+        content.append(throwerDetails(logEntries, Device.THROWER_1));
+        content.append("Wyrzutnik 2\n");
+        content.append(throwerDetails(logEntries, Device.THROWER_2));
+        content.append("Wyrzutnik 3\n");
+        content.append(throwerDetails(logEntries, Device.THROWER_3));
+        content.append("Wyrzutnik 4\n");
+        content.append(throwerDetails(logEntries, Device.THROWER_4));
+        content.append("Wyrzutnik 5\n");
+        content.append(throwerDetails(logEntries, Device.THROWER_5));
+        content.append("Wyrzutnik 6\n");
+        content.append(throwerDetails(logEntries, Device.THROWER_6));
+
+        return content.toString();
+    }
+
+    @SuppressLint("DefaultLocale")
+    private String wallDetails(List<LogEntry> entries, Device wall) {
+        int sum = 0;
+        Map<LogType, Integer> logCount = new HashMap<>();
+
+        for (LogEntry entry : entries) {
+            if (entry.getDevice() == wall) {
+                sum++;
+                logCount.put(entry.getType(), logCount.getOrDefault(entry.getType(), 0) + 1);
+            }
+        }
+
+        if (sum == 0) {
+            return "Brak statystyk.\n";
+        }
+
+        StringBuilder content = new StringBuilder();
+        content.append(" - Suma: ").append(sum).append("\n");
+
+        float correctPercentage = 100f * logCount.getOrDefault(LogType.WALL_CORRECT, 0) / sum;
+        float displacementPercentage = 100f * logCount.getOrDefault(LogType.WALL_DISPLACEMENT, 0) / sum;
+        float otherPercentage = 100f * logCount.getOrDefault(LogType.WALL_OTHER, 0) / sum;
+
+        content.append(" - Poprawnie: ").append(String.format("%.2f", correctPercentage)).append("%\n");
+        content.append(" - Przesunięcie: ").append(String.format("%.2f", displacementPercentage)).append("%\n");
+        content.append(" - Inny błąd: ").append(String.format("%.2f", otherPercentage)).append("%\n");
+
+        return content.append("\n").toString();
+    }
+
+    private String throwerDetails(List<LogEntry> entries, Device wall) {
+        //
+        return "Metoda wymaga implementacji\n";
     }
 }
